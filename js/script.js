def55e35ed3ce154647cff8cc2809cb7be7c4952
shofian46 <shofianal28@gmail.com>
2022-03-01 05:04:17 +0700

    shofian46/game-javascript

--- js/script.js
+++ js/script.js
@@ -1,39 +1,70 @@
 /** @format */
-var tanya = true;
-while (tanya) {
-    // menangkap pilihan player
-    var p = prompt('pilih : gajah, semut, orang');
 
-    // menangkap pilihan computer
-    // membangkitkan bilangan random
-    var comp = Math.random();
-
-    if (comp < 0.34) {
-        comp = 'gajah';
-    } else if (comp >= 0.34 && comp < 0.67) {
-        comp = 'orang';
-    } else {
-        comp = 'semut';
-    }
-
-    var hasil = '';
-    // menentukan rules
-    if (p == comp) {
-        hasil = 'SERI!';
-    } else if (p == 'gajah') {
-        hasil = comp == 'orang' ? 'MENANG!' : 'KALAH!';
-    } else if (p == 'orang') {
-        hasil = comp == 'gajah' ? 'KALAH!' : 'MENANG!';
-    } else if (p == 'orang') {
-        hasil = comp == 'orang' ? 'KALAH!' : 'MENANG';
-    } else {
-        hasil = 'Memasukkan pilihan yang salah!';
-    }
-
-    // tampilkan hasilnya
-    alert('Kamu memilih : ' + p + ' dan Komputer memilih ' + comp + '\nMaka hasilnya : Kamu ' + hasil);
-
-    tanya = confirm('Mau main lagi?');
+function getPilihanKomputer() {
+    const comp = Math.random();
+    if (comp < 0.34) return 'gajah';
+    if (comp >= 0.34 && comp < 0.67) return 'orang';
+    return 'semut';
 }
 
-alert('Terimakasih telah bermain');
+function getHasil(comp, player) {
+    // menentukan rules
+    if (player == comp) return 'SERI!';
+    if (player == 'gajah') return comp == 'orang' ? 'MENANG!' : 'KALAH!';
+    if (player == 'orang') return comp == 'gajah' ? 'KALAH!' : 'MENANG!';
+    if (player == 'semut') return comp == 'orang' ? 'KALAH!' : 'MENANG';
+}
+
+const pGajah = document.querySelector('.gajah');
+pGajah.addEventListener('click', function() {
+    const pilihanComputer = getPilihanKomputer();
+    const pilihanPlayer = pGajah.className;
+    const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+    const imgComputer = document.querySelector('.img-komputer');
+    imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+    const info = document.querySelector('.info');
+    info.innerHTML = hasil;
+});
+
+const pOrang = document.querySelector('.orang');
+pOrang.addEventListener('click', function() {
+    const pilihanComputer = getPilihanKomputer();
+    const pilihanPlayer = pOrang.className;
+    const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+    const imgComputer = document.querySelector('.img-komputer');
+    imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+    const info = document.querySelector('.info');
+    info.innerHTML = hasil;
+});
+
+const pSemut = document.querySelector('.semut');
+pSemut.addEventListener('click', function() {
+    const pilihanComputer = getPilihanKomputer();
+    const pilihanPlayer = pSemut.className;
+    const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+    const imgComputer = document.querySelector('.img-komputer');
+    imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+    const info = document.querySelector('.info');
+    info.innerHTML = hasil;
+});
+// var tanya = true;
+// while (tanya) {
+//     // menangkap pilihan player
+//     var p = prompt('pilih : gajah, semut, orang');
+
+//     // menangkap pilihan computer
+//     // membangkitkan bilangan random
+
+//     // tampilkan hasilnya
+//     alert('Kamu memilih : ' + p + ' dan Komputer memilih ' + comp + '\nMaka hasilnya : Kamu ' + hasil);
+
+//     tanya = confirm('Mau main lagi?');
+// }
+
+// alert('Terimakasih telah bermain');
